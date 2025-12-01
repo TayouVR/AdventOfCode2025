d9834bc17148874cfc94eba1ef5696a3b1b4a34e
Tayou <git@tayou.org>
2025-12-01 14:06:14 +0100

    add Day 1 code

---
+++ Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string rotationString = File.ReadAllText("input.txt");
+
+List<string> rotationList = rotationString.Split('\n').ToList();
+
+int matches = 0;
+int runningNumber = 50;
+string currentSequence = "";
+foreach (string rotation in rotationList) {
+    if (string.IsNullOrEmpty(rotation)) {
+        continue;
+    }
+    int rotationNumber = rotation.StartsWith("L") ? -int.Parse(rotation.Substring(1)) : int.Parse(rotation.Substring(1));
+    
+    runningNumber += rotationNumber;
+    currentSequence += rotation;
+
+    if (runningNumber % 100 == 0) {
+        matches++;
+        Console.WriteLine($"curr: {runningNumber}, count: {matches}, sequence: {currentSequence}");
+        currentSequence = "";
+    }
+}
