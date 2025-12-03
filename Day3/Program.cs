1cdab82fba7c8c5f9fbe849353c78dcf7455cf13
Tayou <git@tayou.org>
2025-12-03 17:28:27 +0100

    add Day 3 code

---
+++ Day3/Program.cs
@@ -0,0 +1,13 @@
+﻿var jolts = new List<int>();
+foreach (var bank in File.ReadLines("input.txt")) {
+    var digits = bank.ToList().ConvertAll(c => Int32.Parse(c.ToString()));
+    var largestDigit = digits.GetRange(0, digits.Count - 1).Max();
+    var largestDigitPosition = digits.IndexOf(largestDigit);
+    var secondDigit = digits.GetRange(largestDigitPosition + 1, digits.Count - (largestDigitPosition + 1)).Max();
+    
+    Console.WriteLine($"{bank}: {largestDigit}{secondDigit}");
+    jolts.Add((largestDigit * 10) + secondDigit);
+}
+
+Console.WriteLine("------------------");
+Console.WriteLine(jolts.Sum());
