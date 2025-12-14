72e98b4402d087b37cc649ed8952cc95b2b456ea
Tayou <git@tayou.org>
2025-12-14 16:18:09 +0100

    add day 5

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var input = File.ReadAllLines("input.txt");
 
-Console.WriteLine("Hello, World!");
+var validRanges = input
+    .Where(line => line
+        .Contains('-')
+    )
+    .Select(line => line
+        .Split('-')
+        .Select(long.Parse)
+        .ToArray()
+    )
+    .ToArray();
+
+var availableIds = input
+    .Where(line => !line.Contains('-') && !string.IsNullOrEmpty(line));
+
+bool IsInRange(long id, long[] range) {
+    return id >= range[0] && id <= range[1];
+}
+
+bool IsInAnyRange(long id, long[][] ranges) {
+    return ranges.Any(range => IsInRange(id, range));
+}
+
+var availableIdIsFresh = 0;
+foreach (var id in availableIds) {
+    availableIdIsFresh += IsInAnyRange(long.Parse(id), validRanges) ? 1 : 0;
+}
+
+Console.WriteLine(availableIdIsFresh);
