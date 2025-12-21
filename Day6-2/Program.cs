11fbb1175c98ccc68a2a067d80ed723bce741fe9
Tayou <git@tayou.org>
2025-12-21 23:22:40 +0100

    add day 6 part 2

--- Day6-2/Program.cs
+++ Day6-2/Program.cs
@@ -1,3 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var input = File.ReadAllLines("input.txt");
 
-Console.WriteLine("Hello, World!");
+static List<(List<string> numbers, char op)> SplitListIntoBlocks(char[][] rows) {
+    List<(List<string> numbers, char op)> outputList = [];
+
+    (List<string> numbers, char op) currentBlock = (numbers: [], op: ' ');
+    foreach (char[] row in rows) {
+        if (row.Length == 0 || string.IsNullOrWhiteSpace(new string(row))) continue;
+        if (row[^1] == '+' || row[^1] == '*') {
+            currentBlock = (numbers: [], op: row[^1]);
+            currentBlock.numbers.Add(new string(row[..^1]));
+            outputList.Add(currentBlock);
+        } else {
+            currentBlock.numbers.Add(new string(row[..^1]));
+        }
+    }
+
+    return outputList;
+}
+
+T[][] Transpose<T>(T[][] matrix)
+{
+    if (matrix == null || matrix.Length == 0) return [];
+    int rows = matrix.Length;
+    int cols = matrix[0].Length;
+    return Enumerable.Range(0, cols)
+        .Select(c => Enumerable.Range(0, rows).Select(r => matrix[r][c]).ToArray())
+        .ToArray();
+}
+
+var inputMap = input.Select(line => line.ToArray()).ToArray();
+
+var data = SplitListIntoBlocks(Transpose(inputMap));
+
+Int64 finalSum = 0;
+foreach (var row in data) {
+    var operation = row.op;
+    Int64 result = 0;
+    Console.WriteLine($"{string.Join(row.op, row.numbers)} = {result}");
+    switch (operation) {
+        case '+': {
+            result = row.numbers.Select(a => Int64.Parse(a)).Aggregate((a, b) => a + b);
+            break;
+        }
+        case '*': {
+            result = row.numbers.Select(a => Int64.Parse(a)).Aggregate((a, b) => a * b);
+            break;
+        }
+    }
+
+    if (row.numbers.Count <= 0) {
+        throw new Exception("No numbers in row");
+    }
+    //Console.WriteLine($"{row.numbers.FirstOrDefault()}{row.op}\n{string.Join('\n', row.numbers[1..])}");
+    finalSum += result;
+}
+
+Console.WriteLine("------------------");
+Console.WriteLine(finalSum);
+
+// my output vs rust output:
+// 59323772873
+// 7450962489289
